f5e97c2eab21ac8c880e303a58d3be2af31966fd
dzamuelis <dzamuelis@gmail.com>
2024-12-16 13:20:10 +0200

    Add files via upload

---
+++ PD02.py
@@ -0,0 +1,34 @@
+# 2. uzdevums
+
+n = int(input("Ievadi skaitli n: "))
+
+summa = 0
+
+for i in range(1, n + 1):
+
+summa n+ = (i)
+
+print(f" {summa}")
+
+# 3. uzdevums
+
+h = int(input("Ievadi skaitli h: "))
+
+reizinajums = 1
+
+for i in range(1, n + 1):
+
+reizinajums *= 1
+
+print(f" {reizinãjums}")
+
+# 4. uzdevums
+
+a = int(input("Ievadi skaitli a: "))
+
+for i in range(a, -1, -1):
+# 5. uzdevums
+let h = int(promt("Ievadit trijstūra augstumu:"))
+for (let i= h;i>=1;):
+    
+    
